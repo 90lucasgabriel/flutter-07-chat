8b4b8d55a01d41cf855efffc309da22083f035bc
Lucas Gabriel <90lucasgabriel@gmail.com>
2021-04-17 20:08:04 -0300

    05 - New: Send message to Firebase;

--- lib/widgets/input.dart
+++ lib/widgets/input.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Input extends StatefulWidget {
+  /**
+   * Send message to Firebase
+   */
+  final Function(String) sendMessage;
+
+  /**
+   * Constructor
+   */
+  Input(this.sendMessage);
+
   @override
   _InputState createState() => _InputState();
 }
 
 class _InputState extends State<Input> {
   bool _isSendEnabled = false;
+  final TextEditingController _messageController = TextEditingController();
+
+  void _reset() {
+    _messageController.clear();
+    setState(() {
+      _isSendEnabled = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,18 +38,27 @@ class _InputState extends State<Input> {
           ),
           Expanded(
             child: TextField(
+              controller: _messageController,
               decoration: InputDecoration.collapsed(hintText: 'Send a message'),
               onChanged: (value) {
                 setState(() {
                   _isSendEnabled = value.isNotEmpty;
                 });
               },
-              // onSubmitted: () {},
+              onSubmitted: (value) {
+                widget.sendMessage(value);
+                _reset();
+              },
             ),
           ),
           IconButton(
             icon: Icon(Icons.send),
-            onPressed: _isSendEnabled ? () {} : null,
+            onPressed: _isSendEnabled
+                ? () {
+                    widget.sendMessage(_messageController.text);
+                    _reset();
+                  }
+                : null,
           ),
         ],
       ),
