5f8dcf9342fb8893988c2be594a3f0271414d189
Lucas Gabriel <90lucasgabriel@gmail.com>
2021-04-17 21:16:13 -0300

    06 - New: Pick image; 06 - New: Send image to Firebase;

--- lib/ui/chat_screen.dart
+++ lib/ui/chat_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
 
 import 'package:chat/widgets/input.dart';
 
@@ -9,8 +11,25 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  void _sendMessage(String value) {
-    FirebaseFirestore.instance.collection('messages').add({'value': value});
+  void _sendMessage({String value, File file}) async {
+    Map<String, dynamic> data = {};
+
+    if (file != null) {
+      UploadTask task = FirebaseStorage.instance
+          .ref()
+          .child(DateTime.now().millisecondsSinceEpoch.toString())
+          .putFile(file);
+
+      TaskSnapshot taskSnapshot = await task.whenComplete(() {});
+      String url = await taskSnapshot.ref.getDownloadURL();
+      data['imageUrl'] = url;
+    }
+
+    if (value != null) {
+      data['value'] = value;
+    }
+
+    FirebaseFirestore.instance.collection('messages').add(data);
   }
 
   @override
